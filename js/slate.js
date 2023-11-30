6dc01c1a7738032fba9891e00466f46c9fc04bd1
leska-ver <lyaisan.333@yandex.ru>
2023-12-01 01:43:18 +0500

    Спринт макет - Производство календарей для фрилансеров

--- js/slate.js
+++ js/slate.js
@@ -39,12 +39,16 @@ document.addEventListener('DOMContentLoaded', function() {
    
 
   // Плавный скролл по якорям. В любое место можно кинуть.
-  $(function(){
-    $('a[href^="#"]').click(function(){
-      var target = $(this).attr('href');
-      $('html, body').animate({scrollTop: $
-    (target).offset().top},800);
-      return false;
-    })
-  })
+  const smoothLinks = document.querySelectorAll('a[href^="#"]');
+  for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = smoothLink.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      });
+    });
+  };
 });  
