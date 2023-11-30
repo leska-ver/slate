02d9ffbf7a7afd108e07459d406846d5b00d1aaa
leska-ver <lyaisan.333@yandex.ru>
2023-12-01 01:29:30 +0500

    Спринт макет - Производство календарей для фрилансеров

--- js/slate.js
+++ js/slate.js
@@ -14,28 +14,37 @@ document.addEventListener('DOMContentLoaded', function() {
     visItems.forEach(function(el) {
       el.classList.add('is-visible')});
     if (visItems.length === itemsLength) {
-        btnMore.closest('.gallery__btn').classList.add('gallery__btn_wrapper--hidden');
+      btnMore.closest('.gallery__btn').classList.add('gallery__btn_wrapper--hidden');
+    }
+  });
+
+
+
+  //Смотреть больше товаров partners//
+  const btnMoreTwo = document.querySelector('.partners__span-more');
+  const itemsLengthTwo = document.querySelectorAll('.partners__item').length; // кол-во карточек //
+  let itemsTwo = 6; // видимое кол-во карточек //
+
+  btnMoreTwo.addEventListener('click', function() {
+    itemsTwo+=6;
+    const arrayTwo = Array.from(document.querySelector('.partners__list').children);
+    const visItemsTwo = arrayTwo.slice(0, itemsTwo);
+    visItemsTwo.forEach(function(el) {
+      el.classList.add('is-visibleTwo')});
+    if (visItemsTwo.length === itemsLengthTwo) {
+        btnMoreTwo.closest('.partners__btn').classList.add('partners__btn_wrapper--hidden');
       }
-    });
-
-
-
-    //Смотреть больше товаров partners//
-    const btnMoreTwo = document.querySelector('.partners__span-more');
-    const itemsLengthTwo = document.querySelectorAll('.partners__item').length; // кол-во карточек //
-    let itemsTwo = 6; // видимое кол-во карточек //
-
-    btnMoreTwo.addEventListener('click', function() {
-      itemsTwo+=6;
-      const arrayTwo = Array.from(document.querySelector('.partners__list').children);
-      const visItemsTwo = arrayTwo.slice(0, itemsTwo);
-      visItemsTwo.forEach(function(el) {
-        el.classList.add('is-visibleTwo')});
-      if (visItemsTwo.length === itemsLengthTwo) {
-          btnMoreTwo.closest('.partners__btn').classList.add('partners__btn_wrapper--hidden');
-        }
-      });
+  });
 
    
 
+  // Плавный скролл по якорям. В любое место можно кинуть.
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      var target = $(this).attr('href');
+      $('html, body').animate({scrollTop: $
+    (target).offset().top},800);
+      return false;
+    })
+  })
 });  
